9f5c96bf414af1bc35b039e3d00b33c36125d843
van <ltvan2304@gmail.com>
2024-12-22 17:18:10 +0700

    update view

--- frontend/src/components/core/HomePage/LearningLanguageSection.jsx
+++ frontend/src/components/core/HomePage/LearningLanguageSection.jsx
@@ -92,7 +92,8 @@ const HeroSection = () => {
                                 sx={{
                                     backgroundColor: "#90EE90",
                                     color: "#ffffff",
-                                    mt: 2,
+                                    // mt: 2,
+                                    mb: 2,
                                     "&:hover": {
                                         backgroundColor: "#76c776",
                                     },
